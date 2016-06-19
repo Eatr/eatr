f75e410f36dc9b77edfb59b446f44ceb655527f7
Lauren Colls <colls.lauren@gmail.com>
2016-06-20 10:04:07 +1200

    modularise getRestaurant function further, cleanupRestaurants func

--- test/unit/google-api.js
+++ test/unit/google-api.js
@@ -5,7 +5,7 @@ import getRestaurants from '../../lib/get-restaurants.js'
 test(' test get-restaurants returns restaurants', function(t) {
 	return getRestaurants(-41.296940, 174.773897, 1000).then((res) => {
 		t.ok(res.length > 1,  "rest array has restaurant objects")
-		t.deepEqual(Object.keys(res[0]), [ 'id', 'name', 'photo', 'open_now', 'location', 'phone', 'address', 'website', 'distance' ], 'keys of rest obs are correct')
+		t.deepEqual(Object.keys(res[0]), [ 'id', 'name', 'photo', 'open_now', 'phone', 'address', 'website', 'distance' ], 'keys of rest obs are correct')
 		t.deepEqual(res[0].photo.substring(0, 4), "http", "photo key has a string value begining http")
 
 	})
