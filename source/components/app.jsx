d20c44a2eb0311b92bff916bf672af1723f99766
Oliver Stigley <oliverstigley@gmail.com>
2016-06-19 20:13:58 +1200

    client and server google request working together

--- source/components/app.jsx
+++ source/components/app.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import * as actionCreators from '../action-creators';
 import { createStore } from 'redux'
-
 import Navbar from './navbar.jsx'
 import Restaurant from './restaurant.jsx'
-
+import getLocation from '../helpers/get-location.js'
+import getRestaurants from '../helpers/get-restaurants.js'
 
 import Splash from './splash.jsx'
 import simulate from '../../lib/simulate-get-restaurants'
@@ -24,12 +24,18 @@ export class App extends Component {
   render() {
  
     if (this.props.preferences.updated) {
-     setTimeout(() => {
-        const newprefs = Object.assign({}, this.props.preferences)
-        newprefs.updated = false
-        console.log('simulating')
-        return this.props.changePreferences(newprefs)},
-      2000)
+      getLocation(this.props.preferences.distance)
+        .then(getRestaurants)
+        .then((restaurants) => {
+          this.props.updateRestaurants(restaurants)
+        })
+        // .then(console.log)
+     // setTimeout(() => {
+     //    const newprefs = Object.assign({}, this.props.preferences)
+     //    newprefs.updated = false
+     //    console.log('simulating')
+     //    return this.props.changePreferences(newprefs)},
+     //  2000)
 
      return (
         <div>
