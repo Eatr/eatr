07290072fcf4ecf70d01f67544f44e15957df4ef
Lauren Colls <colls.lauren@gmail.com>
2016-06-19 18:27:17 +1200

    refactor, move the dotthens to getrestaurant function

--- test/unit/google-api.js
+++ test/unit/google-api.js
@@ -3,13 +3,10 @@ import test from 'blue-tape'
 import getRestaurants from '../../lib/get-restaurants.js'
 
 test(' test get-restaurants returns restaurants', function(t) {
-	return getRestaurants(-41.2865, 174.7762, 500).then((res) => {
-
+	return getRestaurants(-41.296940, 174.773897, 1000).then((res) => {
 		t.ok(res.length > 1,  "rest array has restaurant objects")
 		t.deepEqual(Object.keys(res[0]), [ 'id', 'name', 'photo', 'open_now', 'location', 'phone', 'address', 'website', 'distance' ], 'keys of rest obs are correct')
-		
 		t.deepEqual(res[0].photo.substring(0, 4), "http", "photo key has a string value begining http")
-
 
 	})
 })
