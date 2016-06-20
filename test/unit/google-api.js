48badbf0c421afb72aad699dbf8e896574073131
Lauren Colls <colls.lauren@gmail.com>
2016-06-20 20:20:09 +1200

    test that next page of rests is different to the first page

--- test/unit/google-api.js
+++ test/unit/google-api.js
@@ -44,24 +44,31 @@ const theNextPage= nock ('https://maps.googleapis.com')
 				.get('/maps/api/place/nearbysearch/json?key=' + key + '&location=-41.29694%2C174.773897&pagetoken=def&radius=200&type=restaurant')
 				.reply(200, getNextPageResponse)
 
+var oldRests 
 
 test(' test get-restaurants returns restaurants', function(t) {
 	return getRestaurants(-41.296940, 174.773897, 200).then((res) => {
-		 console.log(res)
+		
 		t.ok(res.restaurantObjects.length > 0,  "rest array has restaurant objects")
 		t.deepEqual(Object.keys(res.restaurantObjects[0]), [ 'id', 'name', 'photo', 'open_now', 'next_page', 'phone', 'address', 'website', 'distance' ], 'keys of rest obs are correct')
 		 t.deepEqual(res.restaurantObjects[0].photo.substring(0, 4), "http", "photo key has a string value begining http")
 
-		let oldRest= res
+		oldRests = res.restaurantObjects
+
 
 	})
 })
 
-test(' test get-restaurants witth next page token returns new restaurants', function(t) {
+test(' get-restaurants wireturns new restaurants when given a new page token', function(t) {
 	return getRestaurants(-41.296940, 174.773897, 200, moreRestsToken).then((res) => {
+ var newRests = res.restaurantObjects
 	t.ok(res.restaurantObjects.length > 0,  "rest array has restaurant objects")
 	t.deepEqual(Object.keys(res.restaurantObjects[0]), [ 'id', 'name', 'photo', 'open_now', 'next_page', 'phone', 'address', 'website', 'distance' ], 'keys of rest obs are correct')
 	 t.deepEqual(res.restaurantObjects[0].photo.substring(0, 4), "http", "photo key has a string value begining http")
+
+	 t.notDeepEqual(oldRests, newRests, "next page of restaurants dooes not equal the old page of rests")
+
+
 	})
 })
 
