f7aeb0895da483c4918e617bba97b3d0fd33a056
Oliver Stigley <oliverstigley@gmail.com>
2016-07-07 22:14:00 +1200

    using enzyme mount for filter-component test, passing

--- test/unit/components/filter-test.js
+++ test/unit/components/filter-test.js
@@ -1,7 +1,12 @@
 import React 			from 'react'
-import {shallow} 	from 'enzyme'
-import {Filter} 		from '../../../source/components/filter.jsx'
+import {shallow, mount} 	from 'enzyme'
+import {Filter, FilterContainer} 		from '../../../source/components/filter.jsx'
+import Navbar 		from '../../../source/components/navbar.jsx'
+import {DistanceContainer} 			from '../../../source/components/distance.jsx'
 import {expect, assert}		from 'chai'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from '../../../source/reducer.js'
 
 describe('The Filter component', () => {
 	const wrapper = shallow(<Filter />)
@@ -14,3 +19,25 @@ describe('The Filter component', () => {
 		expect(wrapper.find('img')).to.have.length(1)
 	})
 })
+
+describe('The Filter component', () => {
+	const store = createStore(reducer)
+	const wrapper = mount(
+		<Provider store={store}>
+	    <Filter />
+	  </Provider>)
+	it('renders subcomponents: Navbar, Distance Container', () => {
+		assert.equal(wrapper.containsAllMatchingElements([
+			<Navbar />,
+			<DistanceContainer />],
+			), true)
+	})
+
+})
+
+
+
+
+
+
+ 
